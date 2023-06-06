3a1c2e263ead71011b54bbdbfa42badff8f35205
damian59 <damianalmanza89@gmail.com>
2023-06-06 00:14:09 -0300

    added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,52 @@
-
+import "../styles/about.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="container">
+      <div className="bubble bubble-left">
+        <h1>Acerca de Mi</h1>
 
-export default About
+        <p className="construccion">
+          Soy un profesional con una sólida experiencia en la industria de la
+          construcción, donde he trabajado durante más de 10 años.
+          Recientemente, decidí dar un cambio de carrera y estoy enfocándome en
+          el desarrollo web. Mi objetivo es combinar mis habilidades en
+          construcción con mis conocimientos en desarrollo web para contribuir a
+          proyectos innovadores en el campo de la construcción.
+        </p>
+        </div>
+        <div className="bubble bubble-right">
+        <h1>Oportunidad</h1>
+        <p className="desarrollo">
+          Durante mi formación en desarrollo web, he adquirido conocimientos en
+          lenguajes de programación como HTML, CSS y JavaScript, así como
+          experiencia en el desarrollo de aplicaciones web utilizando frameworks
+          como React. También tengo habilidades en la gestión de bases de datos
+          y la implementación de soluciones backend utilizando tecnologías como
+          Node.js y MongoDB. Mi experiencia previa en la construcción me ha
+          brindado habilidades en la resolución de problemas, la gestión de
+          proyectos y el trabajo en equipo. Estoy emocionado por esta nueva
+          etapa en mi vida y estoy comprometido con el aprendizaje continuo y el
+          crecimiento en el campo del desarrollo web. Estoy buscando
+          oportunidades que me permitan aplicar mis habilidades y conocimientos
+          en el desarrollo web. Soy una persona dedicada, con una ética laboral
+          sólida y una pasión por aprender. Si deseas más detalles sobre mi
+          experiencia o referencias, no dudes en contactarme.
+        </p>
+        </div>
+     
+      <div className="bubble bubble-left">
+        <h1>Next Challenges</h1>
+        <p className="desafios">
+          Mis próximos desafíos incluyen Python &#128013; y TypeScript. Estos
+          lenguajes son ampliamente utilizados en el desarrollo de software y me
+          permitirán ampliar mis habilidades y explorar nuevas áreas, como la
+          ciencia de datos y el desarrollo web. Estoy emocionado por sumergirme
+          en ellos y aprovechar sus beneficios en mis futuros proyectos.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
